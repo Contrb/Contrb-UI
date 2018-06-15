db39d402fe8b06c1ebbbe43ca65a704f9eb73028
Devin Matte <devinmatte@gmail.com>
2018-06-15 19:41:03 -0400

    Using react router

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './style/Home.css';
+import AppBar from "./AppBar";
+
+class Home extends Component {
+  render() {
+    return (
+      <AppBar />
+    );
+  }
+}
+
+export default Home;
